a559585267e94ede5c17c04e105a348914426c68
ethankim00 <ethan_kim@college.harvard.edu>
2022-08-26 15:40:37 +0000

    add hiearchical mapping for OAG datasets

--- src/mps/datasets/__init__.py
+++ src/mps/datasets/__init__.py
@@ -1,6 +1,6 @@
 from .oag_qa import OAGBeirConverter
 
-BEIR_DATASETS = ["nfcorpus", "fiqa"]
+BEIR_DATASETS = ["nfcorpus", "fiqa", "trec-covid"]
 OAG_DATASETS = ['deep_learning',
  'real_analysis',
  'functional_analysis',
@@ -88,3 +88,52 @@ OAG_DATASETS = ['deep_learning',
  'mathematical_physics',
  'cognitive_science',
  'quantum_information']
+
+
+TOP_LEVEL_OAG_DATASETS = {'GEOMETRY': [ 'geometry','algebraic_geometry','algebraic_topology','differential_geometry',
+'group_theory','category','topology'],
+
+'STATISTICS': ['mathematical_statistics','bayes_theorem','probability_theory'],
+
+'ALGEBRA': ['algebra','polynomial'],
+
+'CALCULUS': ['calculus','partial_differential_equation','functional_analysis','hilbert_space','real_analysis'],
+
+'NUMBER_THEORY': ['number_theory','combinatorics','set_theory','prime_number'],
+
+'LINEAR_ALGEBRA' : ['linear_algebra','matrix'],
+
+'ASTROPHYSICS': ['astronomy','astrophysics','universe','cosmology','general_relativity','special_relativity',
+'spacetime','dark_matter','black_hole','entropy','string_theory'],
+
+'QUANTUM_MECHANICS': ['quantum_gravity','quantum_mechanics','quantum_entanglement','quantum_information','quantum_field_theory',
+'particle_physics','photon','supersymmetry','thermodynamics','experimental_physics','conformal_field_theory','gauge_theory'],
+
+'PHYSICS': ['classical_mechanics','condensed_matter_physics','optics','electromagnetism','mathematical_physics'],
+
+'CHEMISTRY': ['organic_chemistry','chemical_synthesis','inorganic_chemistry','physical_chemistry','computational_chemistry'],
+
+'BIOCHEMISTRY': ['biochemistry','cell_biology'],
+
+'HEALTH_CARE': ['health_care','endocrinology','physiology'],
+
+'NATURAL_SCIENCE': ['natural_science','evolutionary_biology'],
+
+'PSYCHOLOGY': ['social_psychology','cognitive_neuroscience'],
+
+'ALGORITHM': ['algorithm','graph_theory'],
+
+'NEURAL_NETWORK': ['artificial_neural_network','cognitive_science'],
+
+'COMPUTER_VISION': ['computer_vision','computer_graphics_images','convolutional_neural_network'],
+
+'DATA_MINING': ['data_mining','feature_selection','cross_validation','time_series','cluster_analysis'],
+
+'DEEP_LEARNING': ['deep_learning','optimization_algorithm','reinforcement_learning'],
+
+'MACHINE_LEARNING':['machine_learning','hidden_markov_model','classifier','linear_regression'],
+
+'NLP':['natural_language_processing','recurrent_neural_network'],
+
+'ECONOMICS':['economics']
+}
