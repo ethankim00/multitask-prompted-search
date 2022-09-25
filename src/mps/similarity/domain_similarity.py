eebbac9fe8f1b075660dc99a6c57ab2f623ffa73
ethankim00 <ethan_kim@college.harvard.edu>
2022-09-25 15:48:35 +0000

    implement domain similarity

--- src/mps/similarity/domain_similarity.py
+++ src/mps/similarity/domain_similarity.py
@@ -4,7 +4,8 @@ from sentence_transformers import SentenceTransformer
 from itertools import combinations
 from sklearn.metrics.pairwise import cosine_similarity
 from abc import ABC
-
+import json
+from pathlib import Path
 
 class DomainFeatureExtractor(ABC):
     def extract_features():
@@ -18,7 +19,7 @@ class SoftPromptEmbeddingFeatureExtractor(DomainFeatureExtractor):
 
 class UnsupervisedDomainFeatureExtractor(DomainFeatureExtractor):
     def __init__(
-        self, encoder_model: str = "all-MiniLM-L6-v2", data_dir: str = "./data/beir"
+        self, encoder_model: str = "all-MiniLM-L6-v2", data_dir: str = "./data/"
     ):
 
         self.encoder_model = SentenceTransformer(encoder_model)
@@ -29,23 +30,30 @@ class UnsupervisedDomainFeatureExtractor(DomainFeatureExtractor):
         for domain in domains:
             features = self._extract_features(domain)
             result[domain] = features
+            
+        return result
 
     def _extract_features(
-        self, domain: str, data_dir: str = "./data/beir"
+        self, domain: str, data_dir: str = "./data/"
     ) -> np.ndarray:
-        documents = self._load_documents(domain)
-        queries = self._load_queries(domain)
+        documents = self._load_documents(domain, "corpus.jsonl")
+        queries = self._load_documents(domain, "queries.jsonl")
         texts = documents + queries
         return self._encode_texts(texts)
 
-    def _load_documents(self, domain: str, num: int = 100) -> Iterable[str]:
-        pass
 
-    def _load_queries(self, domain: str, num: int = 100) -> Iterable[str]:
+    def _load_documents(self, domain: str, file_name: str, num: int = 100) -> Iterable[str]:
 
-        folder_path = Path(self.data_dir).joinpath("domain")
-        query_path = folder_path.joinpath()
-        document_path = folder_path.joinpath()
+        folder_path = Path(self.data_dir).joinpath(domain)
+        document_path = folder_path.joinpath(file_name)
+        document_list = []
+        with open(document_path, 'r') as json_file:
+            for i, line in enumerate(json_file):
+                if i == num:
+                    break
+                document_list.append(json.loads(line))
+        document_list = [document["text"] for document in document_list]
+        return document_list[0:num]
 
     def _encode_texts(self, texts: List[str]) -> np.ndarray:
         embeddings = self.encoder_model.encode(texts)
@@ -74,23 +82,32 @@ class DomainSimilarity:
         self,
         embeddings: Dict,
     ) -> np.ndarray:
+        similarities = np.zeros((len(self.domains), len(self.domains)))
         for pair in combinations(list(embeddings.keys()), 2):
             similarity = self._calculate_similarities(pair, embeddings)
+            i, j = self.domains.index(pair[0]), self.domains.index(pair[1])
+            similarities[i, j] = similarity
+        return similarities + similarities.T # Fill in similarity Matrix
 
     def _calculate_similarities(
         self, pair: Tuple[str, str], embeddings: np.ndarray
     ) -> float:
         emb1, emb2 = embeddings[pair[0]], embeddings[pair[1]]
         if self.method == "average":
-            return cosine_similarity(np.mean(emb1, axis=0), np.mean(emb2, axis=0))
+            return cosine_similarity(np.mean(emb1, axis=0).reshape(1, -1), np.mean(emb2, axis=0).reshape(1, -1))
         elif self.method == "pairwise":
             return np.mean(cosine_similarity(emb1, emb2))
 
-    def return_most_similar_domains(
+    def return_domain_similarities(
         self, domain: str, k: int = 5
     ) -> List[Tuple[str, float]]:
-        pass
-
+        domain_similarites = [(self.domains[i], score) for i, score in enumerate(self.similarities[self.domains.index(domain)]) if self.domains[i] != domain]
+        if k is not None:
+            print(k)
+            domain_similarites.sort(key = lambda x: x[1], reverse = True)
+            domain_similarites = domain_similarites[0:k]
+        return domain_similarites
+            
 
 class ModelCombiner:
     pass
