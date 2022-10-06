8639b5b4ddab1793ba6348419b1235bc4f8b1d12
Ethan Kim <ethan_kim@college.harvard.edu>
2022-10-06 19:43:15 -0400

    add dataloading logic for separate cqadupstack datasets

--- src/mps/utils.py
+++ src/mps/utils.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass, field
-from src.mps.datasets import OAGBeirConverter, BEIR_DATASETS, OAG_DATASETS
+from src.mps.datasets import OAGBeirConverter, BEIR_DATASETS, OAG_DATASETS, CQA_DATASETS
 from beir import util, LoggingHandler
 
 import os
@@ -12,7 +12,7 @@ class BeirDatasetArguments:
     data_dir: str = "./data"
 
 
-def download_dataset(dataset_args: BeirDatasetArguments):
+def download_dataset(dataset_args: BeirDatasetArguments) -> str:
     if dataset_args.dataset in BEIR_DATASETS:
         url = "https://public.ukp.informatik.tu-darmstadt.de/thakur/BEIR/datasets/{}.zip".format(
             dataset_args.dataset
@@ -24,13 +24,15 @@ def download_dataset(dataset_args: BeirDatasetArguments):
             data_dir=Path(dataset_args.data_dir).joinpath("oag_qa")
         )
         data_dir = converter.convert(dataset_args.dataset)
-
+    elif dataset_args.dataset in CQA_DATASETS:
+        url = "https://public.ukp.informatik.tu-darmstadt.de/thakur/BEIR/datasets/cqadupstack.zip"
+        out_dir = os.path.join(Path("./", dataset_args.data_dir))
+        data_dir = util.download_and_unzip(url, out_dir)
+        data_dir = data_dir + dataset_args.dataset
     # elif dataset_args.dataset in TOP_LEVEL_OAG:
     #     s    pass
-
     # TODO logic for top level oag topics
     # Convert all lower level datasets
     # Compbine in a single directory, concatenative files and adjusting index mappings
     # 1 load
-
     return data_dir
