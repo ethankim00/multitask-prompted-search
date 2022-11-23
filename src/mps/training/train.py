120f3d54fe94b6176649ff1021cb09dbdd316eaa
Ethan Kim <ethan_kim@college.harvard.edu>
2022-11-23 05:00:10 +0000

    finalize openmatch training

--- src/mps/training/train.py
+++ src/mps/training/train.py
@@ -17,7 +17,7 @@ from transformers import (
     T5EncoderModel,
 )
 
-from openmatch.model.linear import LinearHead
+from openmatch.modeling.linear import LinearHead
 
 from openmatch.arguments import DataArguments
 from openmatch.arguments import DRTrainingArguments as TrainingArguments
@@ -34,7 +34,7 @@ from openmatch.trainer import GCDenseTrainer
 from transformers import AutoConfig, AutoTokenizer, HfArgumentParser, set_seed
 
 # from transformers.integrations import TensorBoardCallback
-from torch import Tensor
+from torch import Tensor, nn
 import torch.nn.functional as F
 import torch
 
@@ -51,7 +51,7 @@ class PromptModelArguments(ModelArguments):
     use_delta: bool = False
     soft_prompt_token_number: int = 40
     init_from_vocab: bool = True
-    freeze_plm: bool = False
+    freeze_plm: bool = True
 
 
 from transformers.modeling_outputs import ModelOutput
@@ -76,22 +76,70 @@ def mean_pooling(token_embeddings, attention_mask):
 
 
 class PromptDRModel(DRModel):
+    
+    
+    
+    def __init__(
+        self,
+        lm_q: PreTrainedModel,
+        lm_p: PreTrainedModel,
+        q_delta_model = None,
+        p_delta_model = None,
+        tied: bool = True,
+        feature: str = "last_hidden_state",
+        pooling: str = "first",
+        head_q: nn.Module = None,
+        head_p: nn.Module = None,
+        normalize: bool = False,
+        model_args: ModelArguments = None,
+        data_args: DataArguments = None,
+        train_args: TrainingArguments = None,
+    ):
+        super(PromptDRModel, self).__init__(lm_q = lm_q, lm_p = lm_p)
+
+        self.tied = tied
+        self.lm_q = lm_q
+        self.lm_p = lm_p
+        self.q_delta_model  = q_delta_model 
+        self.p_delta_model  = p_delta_model 
+        self.head_q = head_q
+        self.head_p = head_p
+
+        self.loss_fn = nn.CrossEntropyLoss(reduction="mean")
+
+        self.feature = feature
+        self.pooling = pooling
+        self.normalize = normalize
+
+        self.model_args = model_args
+        self.train_args = train_args
+        self.data_args = data_args
+
+        if train_args is not None and train_args.negatives_x_device:
+            if not dist.is_initialized():
+                raise ValueError(
+                    "Distributed training has not been initialized for representation all gather."
+                )
+            self.process_rank = dist.get_rank()
+            self.world_size = dist.get_world_size()
+    
+    
     def save(self, output_dir: str):
         if not self.tied:
             os.makedirs(os.path.join(output_dir, "query_model"))
             os.makedirs(os.path.join(output_dir, "passage_model"))
             self.q_delta_model.save_finetuned(
-                os.path.join(output_dir, "query_model", "model.ckpt")
+                os.path.join(output_dir, "query_model")
             )
             self.p_delta_model.save_finetuned(
-                os.path.join(output_dir, "passage_model", "model.ckpt")
+                os.path.join(output_dir, "passage_model")
             )
             if self.head_q is not None:
                 self.head_q.save(os.path.join(output_dir, "query_head"))
                 self.head_p.save(os.path.join(output_dir, "passage_head"))
         else:
             self.q_delta_model.save_finetuned(
-                os.path.join(output_dir, "query_model", "model.ckpt")
+                os.path.join(output_dir, "query_model")
             )
             if self.head_q is not None:
                 self.head_q.save(output_dir)
@@ -135,6 +183,26 @@ class PromptDRModel(DRModel):
         if self.normalize:
             reps = F.normalize(reps, dim=1)
         return hidden, reps
+    
+    
+    @staticmethod
+    def _load_delta_model(model_args, model_type: str):
+        with open(os.path.join(model_args.model_name_or_path, model_type, "config.json")) as f:
+            config = json.load(f)
+        model_name = config["backbone_checkpoint_name"]
+        model_class = getattr(
+                    importlib.import_module("transformers"), model_name
+                )
+        lm = model_class.from_pretrained(
+            model_args.model_name_or_path, **hf_kwargs
+        )
+        delta_model = SoftPromptModel(
+            lm,
+            token_init=model_args.init_from_vocab,
+            soft_token_num=model_args.soft_prompt_token_number,
+        )
+        delta_model.from_finetuned(os.path.join(model_args.model_name_or_path, model_type), lm, local_files_only=True)
+        return lm, delta_model 
 
     @classmethod
     def build(
@@ -147,6 +215,7 @@ class PromptDRModel(DRModel):
         # load local
         config = None
         head_q = head_p = None
+        q_delta_model = p_delta_model = None
         if os.path.exists(
             os.path.join(model_args.model_name_or_path, "openmatch_config.json")
         ):
@@ -156,65 +225,55 @@ class PromptDRModel(DRModel):
                 config = json.load(f)
         if (
             os.path.isdir(model_args.model_name_or_path) and config is not None
-        ):  # an OpenMatch model
+        ):  # an OpenMatch model load from directory, still want to load the backbone model from hub or from cache
             tied = config["tied"]
             if tied:
-                logger.info(
-                    f"loading query model weight from {model_args.model_name_or_path}"
-                )
-                model_name = config["plm_backbone"]["type"]
-                model_class = getattr(
-                    importlib.import_module("transformers"), model_name
-                )
-                lm_q = lm_p = model_class.from_pretrained(
-                    model_args.model_name_or_path, **hf_kwargs
-                )
+                lm_q, q_delta_model = self._load_delta_model(model_args, model_type = "query_model")
+                # TODO support linear dimension reduction head
+                lm_p, p_delta_model = lm_q, q_delta_model
                 if config["linear_head"]:
                     head_q = head_p = LinearHead.load(model_args.model_name_or_path)
             else:
-                _qry_model_path = os.path.join(
-                    model_args.model_name_or_path, "query_model"
-                )
-                _psg_model_path = os.path.join(
-                    model_args.model_name_or_path, "passage_model"
-                )
-                _qry_head_path = os.path.join(
-                    model_args.model_name_or_path, "query_head"
-                )
-                _psg_head_path = os.path.join(
-                    model_args.model_name_or_path, "passage_head"
-                )
-                logger.info(f"loading query model weight from {_qry_model_path}")
-                model_name = config["plm_backbone"]["lm_q_type"]
-                model_class = getattr(
-                    importlib.import_module("transformers"), model_name
-                )
-                if os.path.exists(os.path.join(_qry_model_path, "config.json")):
-                    logger.info(f"loading query model config from {_qry_model_path}")
-                    qry_model_config = AutoConfig.from_pretrained(_qry_model_path)
-                    hf_kwargs["config"] = qry_model_config
-                lm_q = model_class.from_pretrained(_qry_model_path, **hf_kwargs)
-
-                logger.info(f"loading passage model weight from {_psg_model_path}")
-                model_name = config["plm_backbone"]["lm_p_type"]
-                model_class = getattr(
-                    importlib.import_module("transformers"), model_name
-                )
-                if os.path.exists(os.path.join(_psg_model_path, "config.json")):
-                    logger.info(f"loading passage model config from {_psg_model_path}")
-                    psg_model_config = AutoConfig.from_pretrained(_psg_model_path)
-                    hf_kwargs["config"] = psg_model_config
-                lm_p = model_class.from_pretrained(_psg_model_path, **hf_kwargs)
+                lm_q, q_delta_model = self._load_delta_model(model_args, model_type = "query_model")
+                lm_p, p_delta_model = self._load_delta_model(model_args, model_type = "passage_model")
 
                 if config["linear_head"]:
                     head_q = LinearHead.load(_qry_head_path)
                     head_p = LinearHead.load(_psg_head_path)
-        else:  # a Huggingface model
+        else:  # a Huggingface model,we are loading from the first time, logic already handled
             tied = not model_args.untie_encoder
             model_class = T5EncoderModel if model_args.encoder_only else AutoModel
             lm_q = model_class.from_pretrained(
                 model_args.model_name_or_path, **hf_kwargs
             )
+            if model_args.use_delta:
+                q_delta_model = SoftPromptModel(
+                    lm_q,
+                    token_init=model_args.init_from_vocab,
+                    soft_token_num=model_args.soft_prompt_token_number,
+                )
+                lm_q.soft_prompt_token_number = model_args.soft_prompt_token_number
+                if model_args.freeze_plm:
+                    q_delta_model.freeze_module(exclude=["deltas"], set_state_dict=True)
+                    q_delta_model.log()
+                if model_args.untie_encoder:
+                    lm_p = model_class.from_pretrained(
+                        model_args.model_name_or_path, **hf_kwargs
+                    )
+                    p_delta_model = SoftPromptModel(
+                        lm_p,
+                        token_init=model_args.init_from_vocab,
+                        soft_token_num=model_args.soft_prompt_token_number,
+                    )
+                    lm_p.soft_prompt_token_number = (
+                        model_args.soft_prompt_token_number
+                    )
+                    if model_args.freeze_plm:
+                        p_delta_model.freeze_module(exclude=["deltas"], set_state_dict=True)
+                else:
+                    lm_p = lm_q      
+            
+            #TODO support linear dimension reduction head
             lm_p = copy.deepcopy(lm_q) if not tied else lm_q
             if model_args.add_linear_head:
                 head_q = LinearHead(
@@ -223,9 +282,11 @@ class PromptDRModel(DRModel):
                 head_p = copy.deepcopy(head_q) if not tied else head_q
 
         model = cls(
+            tied=tied,
             lm_q=lm_q,
             lm_p=lm_p,
-            tied=tied,
+            q_delta_model = q_delta_model,
+            p_delta_model = p_delta_model,
             feature=model_args.feature
             if config is None
             else config["plm_backbone"]["feature"],
@@ -237,33 +298,6 @@ class PromptDRModel(DRModel):
             data_args=data_args,
             train_args=train_args,
         )
-
-        # move up everything that doesn't have to be set on the model
-        if model_args.use_delta:
-            q_delta_model = SoftPromptModel(
-                model.lm_q,
-                token_init=model_args.init_from_vocab,
-                soft_token_num=model_args.soft_prompt_token_number,
-            )
-            model.lm_q.soft_prompt_token_number = model_args.soft_prompt_token_number
-            if model_args.freeze_plm:
-                q_delta_model.freeze_module(exclude=["deltas"], set_state_dict=True)
-                q_delta_model.log()
-                model.q_delta_model = q_delta_model
-            if model_args.untie_encoder:
-                p_delta_model = SoftPromptModel(
-                    model.lm_p,
-                    token_init=model_args.init_from_vocab,
-                    soft_token_num=model_args.soft_prompt_token_number,
-                )
-                model.lm_p.soft_prompt_token_number = (
-                    model_args.soft_prompt_token_number
-                )
-                if model_args.freeze_plm:
-                    p_delta_model.freeze_module(exclude=["deltas"], set_state_dict=True)
-                model.p_delta_model = p_delta_model
-            else:
-                model.lm_p = model.lm_q
         return model
 
 
@@ -331,6 +365,7 @@ def train():
         config=config,
         cache_dir=model_args.cache_dir,
     )
+    model.save("./models/test")
     TrainDatasetClass = (
         MappingDRTrainDataset
         if training_args.use_mapping_dataset
