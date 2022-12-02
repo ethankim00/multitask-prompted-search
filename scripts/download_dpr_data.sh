f4ead5a834358a44d16d7b9dd07b6ebc01906068
Ethan Kim <ethan_kim@college.harvard.edu>
2022-12-01 20:33:28 -0500

    add bash script for all dpr data downloads

---
+++ scripts/download_dpr_data.sh
@@ -0,0 +1,4 @@
+ python ./src/mps/datasets/dpr/download_data.py--resource data.retriever.nq --output_dir ./data/dpr
+python ./src/mps/datasets/dpr/download_data.py--resource data.retriever.trivia --output_dir ./data/dpr
+python ./src/mps/datasets/dpr/download_data.py--resource ddata.retriever.webq --output_dir ./data/dpr
+python ./src/mps/datasets/dpr/download_data.py--resource data.retriever.curatedtre --output_dir ./data/dpr
