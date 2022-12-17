7ab89de127bb5b280de038b5547a1668d499d2c0
Ethan Kim <ethan_kim@college.harvard.edu>
2022-12-17 03:01:32 +0000

    update download data script

--- scripts/download_dpr_data.sh
+++ scripts/download_dpr_data.sh
@@ -1,4 +1,6 @@
- python ./src/mps/datasets/dpr/download_data.py--resource data.retriever.nq --output_dir ./data/dpr
-python ./src/mps/datasets/dpr/download_data.py--resource data.retriever.trivia --output_dir ./data/dpr
-python ./src/mps/datasets/dpr/download_data.py--resource ddata.retriever.webq --output_dir ./data/dpr
-python ./src/mps/datasets/dpr/download_data.py--resource data.retriever.curatedtre --output_dir ./data/dpr
+python ./src/mps/datasets/dpr/download_data.py --resource data.retriever.nq --output_dir ./data/dpr
+python ./src/mps/datasets/dpr/download_data.py --resource data.retriever.trivia --output_dir ./data/dpr
+python ./src/mps/datasets/dpr/download_data.py --resource data.retriever.webq-train --output_dir ./data/dpr
+python ./src/mps/datasets/dpr/download_data.py --resource data.retriever.curatedtrec --output_dir ./data/dpr
+
+#python OpenMatch/scripts/nq-dpr/build_train.py --input ./data/dpr/downloads/data/retriever/curatedtrec-train.json --output ./data/pretraining/curated_trecl.jsonl
