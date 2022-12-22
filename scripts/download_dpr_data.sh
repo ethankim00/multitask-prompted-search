8faa5644921d9195bab2a439c953c0258bd9c5b8
Ethan Kim <ethan_kim@college.harvard.edu>
2022-12-22 00:50:24 +0000

    fix download dpr data script

--- scripts/download_dpr_data.sh
+++ scripts/download_dpr_data.sh
@@ -1,6 +1,7 @@
-python ./src/mps/datasets/dpr/download_data.py --resource data.retriever.nq --output_dir ./data/dpr
-python ./src/mps/datasets/dpr/download_data.py --resource data.retriever.trivia --output_dir ./data/dpr
-python ./src/mps/datasets/dpr/download_data.py --resource data.retriever.webq-train --output_dir ./data/dpr
-python ./src/mps/datasets/dpr/download_data.py --resource data.retriever.curatedtrec --output_dir ./data/dpr
+for dataset in data.retriever.nq data.retriever.trivia data.retriever.webq-train data.retriever.curatedtrec
+    do
+    python ./src/mps/datasets/dpr/download_data.py --resource $dataset --output_dir ./data/dpr
+    done
+    
 
 #python OpenMatch/scripts/nq-dpr/build_train.py --input ./data/dpr/downloads/data/retriever/curatedtrec-train.json --output ./data/pretraining/curated_trecl.jsonl
