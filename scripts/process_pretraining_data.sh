70899ed6025104d5d9a8b778e5a823ea4e7498b8
Ethan Kim <ethan_kim@college.harvard.edu>
2022-12-25 17:48:33 +0000

    fix preprocessing script

--- scripts/process_pretraining_data.sh
+++ scripts/process_pretraining_data.sh
@@ -1,4 +1,4 @@
 for dataset in trivia nq webq curated_trec
     do
-    python OpenMatch/scripts/nq-dpr/build_train.py --input ./data/dpr/downloads/data/retriever/$dataset-train.json --output ./data/pretraining/trivia-qa.jsonl --num_workers 8 --mp_chunk_size 250
+    python OpenMatch/scripts/nq-dpr/build_train.py --input ./data/dpr/downloads/data/retriever/$dataset-train.json --output ./data/pretraining/$dataset.jsonl --num_workers 8 --mp_chunk_size 250
     done
